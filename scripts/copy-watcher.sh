9221bfd86f398b5c8071ca65dbbd19319a47d109
soumyasethy <soumya.sethy@voltmoney.in>
2022-10-02 03:44:35 +0530

    initial commit

--- scripts/copy-watcher.sh
+++ scripts/copy-watcher.sh
@@ -2,7 +2,7 @@
 
 # Trigger on rebuild of lib
 # Copy to BlueLearn App ->  only for dev use
-APP_PATH="../bluelearn-app/node_modules/@blue-learn/micro-frontend"
-NO_CODE_PATH="../no-code-platform/node_modules/@blue-learn/micro-frontend"
-[ -d $APP_PATH ] && cp -R $1 $APP_PATH/$1 && echo "👉 Watcher Copy @blue-learn/micro-frontend -> BlueLearn-App ✅ "
-[ -d $NO_CODE_PATH ] && cp -R $1 $NO_CODE_PATH/$1 && echo "👉 Watcher Copy @blue-learn/micro-frontend -> NO_CODE_PATH ✅ "
+APP_PATH="../volt-app/node_modules/@volt/micro-frontend"
+NO_CODE_PATH="../no-code-platform/node_modules/@volt/micro-frontend"
+[ -d $APP_PATH ] && cp -R $1 $APP_PATH/$1 && echo "👉 Watcher Copy @volt/micro-frontend -> Volt-App ✅ "
+[ -d $NO_CODE_PATH ] && cp -R $1 $NO_CODE_PATH/$1 && echo "👉 Watcher Copy @volt/micro-frontend -> NO_CODE_PATH ✅ "
