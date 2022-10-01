f10432010c5c6302dc63de32aa494fea78ecc83b
soumyasethy <soumya.sethy@voltmoney.in>
2022-10-02 03:46:28 +0530

    initial commit

--- scripts/copy-dev.sh
+++ scripts/copy-dev.sh
@@ -1,14 +1,14 @@
 BUILD_PATH="lib"
-echo "Copy @blue-learn/platform ⏳ "
+echo "Copy @volt/platform ⏳ "
 
 # 1.Copy to BlueLearn App ->  only for dev use
-APP_PATH="../bluelearn-app/node_modules/@blue-learn/micro-frontend"
+APP_PATH="../volt-app/node_modules/@volt/micro-frontend"
 [ -d $APP_PATH ] && rm -rf $APP_PATH/"lib" && echo "👉 Clear -> $APP_PATH/lib ✅ "
 mkdir -p $APP_PATH/"lib" && echo "👉 Link -> $APP_PATH/lib ✅ " #&& cd .. #uncomment when run local
-[ -d $APP_PATH ] && cp -R $BUILD_PATH $APP_PATH && echo "👉 Copy @blue-learn/micro-frontend -> BlueLearn-App ✅ "
+[ -d $APP_PATH ] && cp -R $BUILD_PATH $APP_PATH && echo "👉 Copy @volt/micro-frontend -> Volt-App ✅ "
 
 
-NO_CODE_PATH="../no-code-platform/node_modules/@blue-learn/micro-frontend"
-[ -d $NO_CODE_PATH ] && rm -rf $NO_CODE_PATH/"lib" && echo "👉 Clear -> $NO_CODE_PATH/lib ✅ "
-mkdir -p $NO_CODE_PATH/"lib" && echo "👉 Link -> $NO_CODE_PATH/lib ✅ " #&& cd .. #uncomment when run local
-[ -d $NO_CODE_PATH ] && cp -R $BUILD_PATH $NO_CODE_PATH && echo "👉 Copy @blue-learn/micro-frontend -> NO_CODE_PATH ✅ "
+#NO_CODE_PATH="../no-code-platform/node_modules/@volt/micro-frontend"
+#[ -d $NO_CODE_PATH ] && rm -rf $NO_CODE_PATH/"lib" && echo "👉 Clear -> $NO_CODE_PATH/lib ✅ "
+#mkdir -p $NO_CODE_PATH/"lib" && echo "👉 Link -> $NO_CODE_PATH/lib ✅ " #&& cd .. #uncomment when run local
+#[ -d $NO_CODE_PATH ] && cp -R $BUILD_PATH $NO_CODE_PATH && echo "👉 Copy @volt/micro-frontend -> NO_CODE_PATH ✅ "
--- scripts/copy-watcher.sh
+++ scripts/copy-watcher.sh
@@ -5,4 +5,4 @@
 APP_PATH="../volt-app/node_modules/@volt/micro-frontend"
 NO_CODE_PATH="../no-code-platform/node_modules/@volt/micro-frontend"
 [ -d $APP_PATH ] && cp -R $1 $APP_PATH/$1 && echo "👉 Watcher Copy @volt/micro-frontend -> Volt-App ✅ "
-[ -d $NO_CODE_PATH ] && cp -R $1 $NO_CODE_PATH/$1 && echo "👉 Watcher Copy @volt/micro-frontend -> NO_CODE_PATH ✅ "
+#[ -d $NO_CODE_PATH ] && cp -R $1 $NO_CODE_PATH/$1 && echo "👉 Watcher Copy @volt/micro-frontend -> NO_CODE_PATH ✅ "
