6547b14c3b9ec131818b05739c8eb430db91c472
soumyasethy <soumya.sethy@voltmoney.in>
2022-10-04 16:46:45 +0530

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { onboardMF } from "./features/on_boarding";
-import {RouteMap} from "@voltmoney/types";
+import { onBoardingMF } from "./features/on_boarding";
+import { RouteMap } from "@voltmoney/types";
+import {signupMf} from "./features/signup";
 
 export enum ROUTE {
-  ON_BOARDING,
-  MOBILE_NO_VERIFY,
-  ENTER_OTP,
-  EMAIL_VERIFY,
-  PAN_VERIFY,
-  PAN_CONFIRM,
+  ON_BOARDING = "ON_BOARDING",
+  SIGNUP = "SIGNUP",
+  MOBILE_NO_VERIFY = "MOBILE_NO_VERIFY",
+  ENTER_OTP = "ENTER_OTP",
+  EMAIL_VERIFY = "EMAIL_VERIFY",
+  PAN_VERIFY = "PAN_VERIFY",
+  PAN_CONFIRM = "PAN_CONFIRM",
 }
-export const ROUTE_MAP:RouteMap = {
-  ON_BOARDING: onboardMF,
+export const ROUTE_MAP: RouteMap = {
+  [ROUTE.ON_BOARDING]: onBoardingMF,
+  [ROUTE.SIGNUP]: signupMf,
 };
