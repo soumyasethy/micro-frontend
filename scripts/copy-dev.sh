03cfca1440af8253a87aa04c84ddbbce557a276f
soumyasethy <soumya.sethy@voltmoney.in>
2022-10-04 11:54:23 +0530

    add watcher, copy script

--- scripts/copy-dev.sh
+++ scripts/copy-dev.sh
@@ -1,8 +1,8 @@
 BUILD_PATH="lib"
-echo "Copy voltmoney/micro-frontend ⏳ "
+echo "Copy @voltmoney/micro-frontend ⏳ "
 
 # 1.Copy to Volt App ->  only for dev use
-APP_PATH="../volt-app/node_modules/voltmoney/micro-frontend"
+APP_PATH="../volt-app/node_modules/@voltmoney/micro-frontend"
 [ -d $APP_PATH ] && rm -rf $APP_PATH/"lib" && echo "👉 Clear -> $APP_PATH/lib ✅ "
 mkdir -p $APP_PATH/"lib" && echo "👉 Link -> $APP_PATH/lib ✅ " #&& cd .. #uncomment when run local
 [ -d $APP_PATH ] && cp -R $BUILD_PATH $APP_PATH && echo "👉 Copy voltmoney/micro-frontend -> Volt-App ✅ "
--- src/features/on_boarding/index.ts
+++ src/features/on_boarding/index.ts
@@ -1,16 +1,54 @@
-import { LAYOUTS, PageType, POSITION, TemplateSchema } from "@voltmoney/types";
-import { ButtonTypeTokens, WIDGET } from "@voltmoney/schema";
+import {
+  Datastore,
+  Layout,
+  LAYOUTS,
+  PageType,
+  POSITION,
+  TemplateSchema,
+} from "@voltmoney/types";
+import {
+  ButtonProps,
+  ButtonTypeTokens,
+  ButtonWidthTypeToken,
+  IconTokens,
+  keyboardTypeToken,
+  TextInputProps,
+  TypographyProps,
+  WIDGET,
+} from "@voltmoney/schema";
 
 export const template: TemplateSchema = {
-  layout: {
+  layout: <Layout>{
     id: "PAGE_NAME",
     type: LAYOUTS.LIST,
     widgets: [
-      { id: "text", type: WIDGET.BUTTON, position: POSITION.FIXED_BOTTOM },
+      { id: "back", type: WIDGET.BUTTON, position: POSITION.FIXED_TOP },
+      { id: "continue", type: WIDGET.BUTTON, position: POSITION.FIXED_BOTTOM },
+      { id: "title", type: WIDGET.TEXT },
+      { id: "subTitle", type: WIDGET.TEXT },
+      { id: "input", type: WIDGET.INPUT },
     ],
   },
-  datastore: {
-    text: { label: "hello world", type: ButtonTypeTokens.LargeElevated },
+  datastore: <Datastore>{
+    back: <ButtonProps>{
+      // label: "back",
+      type: ButtonTypeTokens.LargeElevated,
+      icon: { name: IconTokens.Back },
+    },
+    continue: <ButtonProps>{
+      label: "continue",
+      type: ButtonTypeTokens.LargeElevated,
+      width: ButtonWidthTypeToken.FULL,
+    },
+    title: <TypographyProps>{ label: "Verify your email id" },
+    subTitle: <TypographyProps>{
+      label: "Your Volt app will only work with this email id",
+    },
+    input: <TextInputProps>{
+      caption: "Enter your other email",
+      title: "Email id",
+      keyboardType: keyboardTypeToken.email,
+    },
   },
 };
 
