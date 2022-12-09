d9f0583b7b370f931004c48f78a6be2ee178c99b
Shekhorroy100 <shekhorroy100@gmail.com>
2022-12-09 17:50:26 +0530

    Debashish/New_Screen_Added_Dist_Basic_Details

---
+++ src/features/Dist_basicDetails/basicDetailsInfo/types.ts
@@ -0,0 +1,17 @@
+export enum ACTION {
+    GO_BACK = "GO_BACK",
+    CHANGE_INPUT = "CHANGE_INPUT",
+    DISABLE_CONTINUE = "DISABLE_CONTINUE",
+    ENABLE_CONTINUE = "ENABLE_CONTINUE",
+    TRIGGER_CTA = "TRIGGER_CTA",
+}
+
+export type InputPayload = {
+    value: string;
+    widgetId: string;
+  };
+
+  export type EnableDisableCTA = {
+    value: boolean;
+    targetWidgetId: string;
+  };
