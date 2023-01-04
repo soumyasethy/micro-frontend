8fbddf0f7bdb0f470b3e6afb339fdf0196644cdf
Shekhorroy100 <shekhorroy100@gmail.com>
2023-01-04 15:40:09 +0530

    Debashish/AddedSetTimeOut

--- src/features/test_page/actions.ts
+++ src/features/test_page/actions.ts
@@ -15,52 +15,54 @@ export const TestAction: ActionFunction<any> = async (
   console.warn("**** Test Action Triggered ****", action);
   importScript(ImportScriptSrc.DIGIO_SCRIPT, getDigio)
   // Digio Kyc Mandate
-  const user: User = await SharedPropsService.getUser();
-  const applicationId = user.linkedApplications[0].applicationId;
-  await network.post(
-      `${api.digioKycESignInitiateRequest}${applicationId}`,
-      {},
-      {
-        headers: {
-          "X-AppMode": "SDK_INVESTWELL",
-          "X-AppPlatform": await SharedPropsService.getAppPlatform(),
-          Authorization: `Bearer ${await SharedPropsService.getToken()}`,
-          "Content-Type": "application/json",
-        }
-      }
-  ).then(response => {
-      if(response.status === 200) {
-         if(response.data && response.data.stepResponseObject) {
-             const requestId = response.data.stepResponseObject.requestId
-             const tokenId = response.data.stepResponseObject.tokenId
-             //@ts-ignore
-             digio.init();
-             console.log("RequestId: ", requestId)
-             console.log("PhoneNumber: ", user.user.phoneNumber)
-             console.log("tokenId: ", tokenId)
-             digio.submit(requestId, user.user.phoneNumber, tokenId)
-             //polling
-             const pollInterval = setInterval(async ()=> {
-                network.post(
-                    `${api.digioKycCheckRequestStatus}${applicationId}`,
-                    {},
-                    {
-                        headers: {
-                            "X-AppMode": "SDK_INVESTWELL",
-                            "X-AppPlatform": await SharedPropsService.getAppPlatform(),
-                            Authorization: `Bearer ${await SharedPropsService.getToken()}`,
-                            "Content-Type": "application/json",
-                        }
-                    }
-                ).then(res=> {
-                    if(res && res.data && res.data.stepResponseObject !== "PENDING") {
-                        clearInterval(pollInterval)
-                    }
-                })
-             }, 5000)
-         }
-      }
-  }).catch(error=>{
-      console.log(error)
-  });
+  setTimeout(async()=> {
+      const user: User = await SharedPropsService.getUser();
+      const applicationId = user.linkedApplications[0].applicationId;
+      await network.post(
+          `${api.digioKycESignInitiateRequest}${applicationId}`,
+          {},
+          {
+              headers: {
+                  "X-AppMode": "SDK_INVESTWELL",
+                  "X-AppPlatform": await SharedPropsService.getAppPlatform(),
+                  Authorization: `Bearer ${await SharedPropsService.getToken()}`,
+                  "Content-Type": "application/json",
+              }
+          }
+      ).then(response => {
+          if(response.status === 200) {
+              if(response.data && response.data.stepResponseObject) {
+                  const requestId = response.data.stepResponseObject.requestId
+                  const tokenId = response.data.stepResponseObject.tokenId
+                  //@ts-ignore
+                  digio.init();
+                  console.log("RequestId: ", requestId)
+                  console.log("PhoneNumber: ", user.user.phoneNumber)
+                  console.log("tokenId: ", tokenId)
+                  digio.submit(requestId, user.user.phoneNumber, tokenId)
+                  //polling
+                  const pollInterval = setInterval(async ()=> {
+                      network.post(
+                          `${api.digioKycCheckRequestStatus}${applicationId}`,
+                          {},
+                          {
+                              headers: {
+                                  "X-AppMode": "SDK_INVESTWELL",
+                                  "X-AppPlatform": await SharedPropsService.getAppPlatform(),
+                                  Authorization: `Bearer ${await SharedPropsService.getToken()}`,
+                                  "Content-Type": "application/json",
+                              }
+                          }
+                      ).then(res=> {
+                          if(res && res.data && res.data.stepResponseObject !== "PENDING") {
+                              clearInterval(pollInterval)
+                          }
+                      })
+                  }, 5000)
+              }
+          }
+      }).catch(error=>{
+          console.log(error)
+      });
+  }, 250)
 };
