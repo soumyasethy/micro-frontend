ae8d2f7c0a59e1506750d655e4458e2ca4983de6
Shekhorroy100 <shekhorroy100@gmail.com>
2023-01-16 17:30:56 +0530

    Debashish/Added_WidgetItems

--- src/features/mfPledge/unlock_limit_landing_new/index.ts
+++ src/features/mfPledge/unlock_limit_landing_new/index.ts
@@ -11,20 +11,23 @@ import {
   SizeTypeTokens,
   SpaceProps,
   StackAlignItems,
+  StackJustifyContent,
   StackProps,
   StackType,
   TypographyProps,
   WIDGET,
 } from "@voltmoney/schema";
 import {ROUTE} from "../../../routes";
-import {ACTION} from "./types";
+import {ACTION, StepResponseObject} from "./types";
 import {continueLimit} from "./actions";
 import {fetchPledgeLimitRepo} from "./repo";
 import SharedPropsService from "../../../SharedPropsService";
 import {AuthCASModel} from "../../../types/AuthCASModel";
+import {portfolioListDatastoreBuilder} from "../portfolio_readonly/utils";
 
-export const template: (availableCreditAmount: number) => TemplateSchema = (
-  availableCreditAmount
+export const template: (availableCreditAmount: number, stepResponseObject: StepResponseObject) => Promise<TemplateSchema> = async(
+  availableCreditAmount,
+  stepResponseObject
 ) => ({
   layout: <Layout>{
     id: ROUTE.UNLOCK_LIMIT_LANDING_NEW,
@@ -43,41 +46,41 @@ export const template: (availableCreditAmount: number) => TemplateSchema = (
     ],
   },
   datastore: <Datastore>{
-    congratulationsText: <TypographyProps> {
+    congratulationsText: <TypographyProps>{
       label: 'Congratulations!',
       fontSize: FontSizeTokens.XL,
       fontFamily: FontFamilyTokens.Poppins,
       fontWeight: '700',
       lineHeight: 28,
     },
-    titleText: <TypographyProps> {
+    titleText: <TypographyProps>{
       label: 'Your portfolio was evaluated successfully',
       fontSize: FontSizeTokens.MD,
       fontFamily: FontFamilyTokens.Inter,
       fontWeight: '400',
       lineHeight: 20,
     },
-    amountCard: <LimitCardProps> {
+    amountCard: <LimitCardProps>{
       isView: false,
-      limitAmount: '10000',
+      limitAmount: `${availableCreditAmount}`,
       beforeOutOf: '30000',
       afterOutOf: '60000',
       label: 'Available credit limit',
     },
-    continue: <ButtonProps> {
+    continue: <ButtonProps>{
       label: 'continue',
       type: ButtonTypeTokens.LargeFilled,
       width: ButtonWidthTypeToken.FULL,
     },
-    otherSourceStack: <StackProps> {
+    otherSourceStack: <StackProps>{
       type: StackType.column,
       alignItems: StackAlignItems.center,
       widgetItems: [
-        { id: 'infoText', type: WIDGET.TEXT },
-        { id: 'infoButton', type: WIDGET.BUTTON }
+        {id: 'infoText', type: WIDGET.TEXT},
+        {id: 'infoButton', type: WIDGET.BUTTON}
       ]
     },
-    infoText: <TypographyProps> {
+    infoText: <TypographyProps>{
       label: 'Don’t see all your mutual funds?',
       fontSize: FontSizeTokens.SM,
       fontFamily: FontFamilyTokens.Inter,
@@ -85,16 +88,57 @@ export const template: (availableCreditAmount: number) => TemplateSchema = (
       color: ColorTokens.Grey_Charcoal,
       lineHeight: 20,
     },
-    infoButton: <ButtonProps> {
+    infoButton: <ButtonProps>{
       label: 'Get from other sources?',
       type: ButtonTypeTokens.MediumGhost,
       width: ButtonWidthTypeToken.FULL,
     },
-    bottomSheet: <BottomSheetProps> {
-      widgetItems:[
-         
+    bottomSheet: <BottomSheetProps>{
+      widgetItems: [
+        { id:'bottomSheetText', type: WIDGET.TEXT },
+        { id: 'bs_space0', type: WIDGET.SPACE },
+        { id:'infoRow', type: WIDGET.STACK },
+        { id: 'bs_space1', type: WIDGET.SPACE },
+        { id:'listItem', type: WIDGET.LIST },
+        { id: 'bs_space2', type: WIDGET.SPACE },
       ]
     },
+    bs_space0: <SpaceProps> { size: SizeTypeTokens.XL },
+    bs_space1: <SpaceProps> { size: SizeTypeTokens.MD },
+    bs_space2: <SpaceProps> { size: SizeTypeTokens.MD },
+    bottomSheetText: <TypographyProps> {
+      label: 'Credit limit on basis of these assets',
+      fontSize: FontSizeTokens.MD,
+      fontFamily: FontFamilyTokens.Inter,
+      fontWeight: '700',
+      color: ColorTokens.Grey_Night,
+      lineHeight: 24,
+    },
+    infoRow: <StackProps> {
+      type: StackType.row,
+      justifyContent: StackJustifyContent.spaceBetween,
+      widgetItems: [
+        { id:"col1Header", type: WIDGET.TEXT },
+        { id:"col2Header", type: WIDGET.TEXT }
+      ]
+    },
+    col1Header: <TypographyProps> {
+      label: 'Asset details',
+      fontSize: FontSizeTokens.XS,
+      fontFamily: FontFamilyTokens.Inter,
+      fontWeight: '500',
+      color: ColorTokens.Grey_Charcoal,
+      lineHeight: 18,
+    },
+    col2Header: <TypographyProps> {
+      label: 'Credit limit',
+      fontSize: FontSizeTokens.XS,
+      fontFamily: FontFamilyTokens.Inter,
+      fontWeight: '500',
+      color: ColorTokens.Grey_Charcoal,
+      lineHeight: 18,
+    },
+    ...(await portfolioListDatastoreBuilder(stepResponseObject)),
     topSpace: <SpaceProps> { size: SizeTypeTokens.XXXXL },
     space1: <SpaceProps> { size: SizeTypeTokens.XXXL },
     space2: <SpaceProps> { size: SizeTypeTokens.XL },
@@ -104,13 +148,19 @@ export const template: (availableCreditAmount: number) => TemplateSchema = (
 
 export const unlockLimitLandingNewMF: PageType<any> = {
   onLoad: async () => {
+    const updateAvailableCASMap = {};
     const authCAS: AuthCASModel = await SharedPropsService.getAuthCASResponse();
     const responseX = authCAS ? authCAS : await fetchPledgeLimitRepo();
-    const availableCreditAmount: number =
-      responseX.stepResponseObject.availableCreditAmount || 0;
+    const stepResponseObject = responseX.stepResponseObject
+    const availableCreditAmount: number = responseX.stepResponseObject.availableCreditAmount || 0;
     /*** disable this page for next time ***/
-    await SharedPropsService.setPledgeFirstTime(false);
-    return Promise.resolve(template(availableCreditAmount));
+    stepResponseObject.availableCAS.map((item, index) => {
+      let key = `${item.isinNo}-${item.folioNo}`;
+      item.pledgedUnits = item.totalAvailableUnits;
+      updateAvailableCASMap[key] = item;
+    });
+    await SharedPropsService.setAvailableCASMap(updateAvailableCASMap);
+    return Promise.resolve(template(availableCreditAmount, stepResponseObject));
   },
 
   actions: {
