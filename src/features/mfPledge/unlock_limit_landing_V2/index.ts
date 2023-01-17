3527964361859c0ead88c11d726ff677d30df80a
Shekhorroy100 <shekhorroy100@gmail.com>
2023-01-17 20:13:51 +0530

    Debashish/Added_KarvyFetch_OTP

--- src/features/mfPledge/unlock_limit_landing_V2/index.ts
+++ src/features/mfPledge/unlock_limit_landing_V2/index.ts
@@ -6,14 +6,14 @@ import {
   ButtonWidthTypeToken,
   ColorTokens,
   FontFamilyTokens,
-  FontSizeTokens,
+  FontSizeTokens, IconTokens,
   LimitCardProps,
   SizeTypeTokens,
   SpaceProps,
   StackAlignItems,
   StackJustifyContent,
   StackProps,
-  StackType,
+  StackType, StepperStateToken,
   TypographyProps,
   WIDGET,
 } from "@voltmoney/schema";
@@ -24,10 +24,30 @@ import {fetchPledgeLimitRepo} from "./repo";
 import SharedPropsService from "../../../SharedPropsService";
 import {AuthCASModel} from "../../../types/AuthCASModel";
 import {portfolioListDatastoreBuilder} from "../portfolio_readonly/utils";
+import {StepStatusMap, User} from "../../login/otp_verify/types";
+import {AvailableCASItem} from "../unlock_limit/types";
+import {api} from "../../../configs/api";
+import {APP_CONFIG, getAppHeader} from "../../../configs/config";
+import {NavigationNext} from "../../kyc/kyc_init/types";
+import {isMorePortfolioRenderCheck} from "../../../configs/utils";
+import _ from "lodash";
+import { getMoreMfPortfolio } from "./actions";
+import { removeGetMorePortfolio } from "./actions";
 
-export const template: (availableCreditAmount: number, stepResponseObject: StepResponseObject) => Promise<TemplateSchema> = async(
-  availableCreditAmount,
-  stepResponseObject
+/*** This will be used to auto trigger removeGetMorePortfolio action when user has already pledged both CAMS and KARVY from UI */
+let availableCASX: AvailableCASItem[];
+
+export const template: (availableCreditAmount: number,
+                        availableCAS: AvailableCASItem[],
+                        stepResponseObject: StepResponseObject,
+                        shouldShowGetMorePortfolio: boolean,
+                        totalPortfolioAmount: number
+) => Promise<TemplateSchema> = async(
+    availableCreditAmount,
+    availableCAS,
+    stepResponseObject,
+    shouldShowGetMorePortfolio,
+    totalPortfolioAmount
 ) => ({
   layout: <Layout>{
     id: ROUTE.UNLOCK_LIMIT_LANDING,
@@ -93,10 +113,17 @@ export const template: (availableCreditAmount: number, stepResponseObject: StepR
       color: ColorTokens.Grey_Charcoal,
       lineHeight: 20,
     },
-    infoButton: <ButtonProps>{
+    infoButton: <ButtonProps & WidgetProps>{
       label: 'Get from other sources?',
       type: ButtonTypeTokens.MediumGhost,
-      width: ButtonWidthTypeToken.FULL,
+      width: ButtonWidthTypeToken.CONTENT,
+      action: {
+        type: ACTION.GET_MORE_MF_PORTFOLIO,
+        routeId: ROUTE.UNLOCK_LIMIT_LANDING,
+        payload: {
+          casList: stepResponseObject.availableCAS
+        }
+      }
     },
     bottomSheet: <BottomSheetProps>{
       widgetItems: [
@@ -152,23 +179,108 @@ export const template: (availableCreditAmount: number, stepResponseObject: StepR
 });
 
 export const unlockLimitLandingMFV2: PageType<any> = {
-  onLoad: async () => {
+  onLoad: async ({ showPopup, network, goBack }) => {
     const updateAvailableCASMap = {};
+    const user: User = await SharedPropsService.getUser();
     const authCAS: AuthCASModel = await SharedPropsService.getAuthCASResponse();
-    const responseX = authCAS ? authCAS : await fetchPledgeLimitRepo();
-    const stepResponseObject = responseX.stepResponseObject
-    const availableCreditAmount: number = responseX.stepResponseObject.availableCreditAmount || 0;
+    const pledgeLimitResponse = authCAS
+        ? { data: authCAS }
+        : await fetchPledgeLimitRepo().then((response) => ({
+          data: response,
+        }));
+    /* const pledgeLimitResponse = await network.get(
+      `${api.pledgeLimit}${user.linkedApplications[0].applicationId}`,
+      { headers: await getAppHeader() }
+    );*/
+    /*** update authCAS in SharedPropsService if fetched from api ***/
+    if (!authCAS && pledgeLimitResponse.data) {
+      await SharedPropsService.setAuthCASResponse(pledgeLimitResponse.data);
+    }
+    const availableCreditAmount: number =
+        pledgeLimitResponse.data.stepResponseObject.availableCreditAmount || 0;
+    const totalPortfolioAmount: number =
+        pledgeLimitResponse.data.stepResponseObject.totalPortfolioAmount || 0;
+
+    const availableCAS: AvailableCASItem[] =
+        pledgeLimitResponse.data.stepResponseObject.availableCAS || [];
+    await SharedPropsService.setCasListOriginal(availableCAS);
+    const stepResponseObject = pledgeLimitResponse.data.stepResponseObject;
+
+    /*** Show popup as soon as we land here if MF_PLEDGE_PORTFOLIO is PENDING_CALLBACK ***/
+    const stepStatusMap: StepStatusMap =
+        pledgeLimitResponse.data.updatedApplicationObj.stepStatusMap;
+    if (
+        stepStatusMap.MF_PLEDGE_PORTFOLIO === StepperStateToken.PENDING_CALLBACK
+    ) {
+      setTimeout(async () => {
+        await showPopup({
+          title: `Pledging...`,
+          subTitle: "Please wait while we confirm your pledge with depository.",
+          type: "DEFAULT",
+          iconName: IconTokens.Volt,
+        });
+      }, 250);
+
+      /***** Starting Polling to check status of MF_PLEDGE_PORTFOLIO *****/
+      const PollerRef = setInterval(async () => {
+        const mfPledgeStatusResponse = await network.get(
+            `${api.borrowerApplication}${user.linkedApplications[0].applicationId}`,
+            { headers: await getAppHeader() }
+        );
+        user.linkedApplications[0] = _.get(mfPledgeStatusResponse, "data");
+        await SharedPropsService.setUser(user);
+        if (
+            _.get(
+                mfPledgeStatusResponse,
+                "data.stepStatusMap.MF_PLEDGE_PORTFOLIO"
+            ) === "COMPLETED" &&
+            _.get(mfPledgeStatusResponse, "data.currentStepId") !==
+            "MF_PLEDGE_PORTFOLIO"
+        ) {
+          clearInterval(PollerRef);
+          await goBack();
+          await showPopup({
+            autoTriggerTimerInMilliseconds: APP_CONFIG.POLLING_INTERVAL,
+            isAutoTriggerCta: true,
+            title: "Limit unlocked successfully!",
+            subTitle: "You will be redirected to next step in few seconds",
+            type: "SUCCESS",
+            ctaLabel: "Continue",
+            primary: true,
+            ctaAction: {
+              type: ACTION.NAV_NEXT,
+              routeId: ROUTE.MF_PLEDGE_PORTFOLIO,
+              payload: <NavigationNext>{
+                stepId: _.get(mfPledgeStatusResponse, "data.currentStepId"),
+              },
+            },
+          });
+        }
+      }, APP_CONFIG.POLLING_INTERVAL);
+    }
+    const isGetMorePortfolio = await isMorePortfolioRenderCheck();
+
+    /*** Prefill pledgeUnit ***/
     stepResponseObject.availableCAS.map((item, index) => {
       let key = `${item.isinNo}-${item.folioNo}`;
       item.pledgedUnits = item.totalAvailableUnits;
       updateAvailableCASMap[key] = item;
     });
     await SharedPropsService.setAvailableCASMap(updateAvailableCASMap);
-    return Promise.resolve(template(availableCreditAmount, stepResponseObject));
+
+    return Promise.resolve(template(
+        availableCreditAmount,
+        availableCASX,
+        stepResponseObject,
+        isGetMorePortfolio,
+        totalPortfolioAmount
+    ));
   },
 
   actions: {
     [ACTION.UNLOCK_LIMIT]: continueLimit,
+    [ACTION.GET_MORE_MF_PORTFOLIO]: getMoreMfPortfolio,
+    [ACTION.REMOVE_GET_MORE_MF_PORTFOLIO]: removeGetMorePortfolio,
   },
   clearPrevious: true,
 };
