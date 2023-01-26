c886f70deaf7b8a3c405257c75ff36ae26d9ce94
Nakshatra Singh <nakshatradsu@gmail.com>
2023-01-26 21:49:06 +0530

    fixed onEdit functionality bug

--- src/features/mfPledge/modify_pledged_amount/actions.ts
+++ src/features/mfPledge/modify_pledged_amount/actions.ts
@@ -1,13 +1,15 @@
 import { ActionFunction, WidgetProps } from "@voltmoney/types";
 import { ROUTE } from "../../../routes";
 import { AmountPayload, ModifyAmountPayload } from "./types";
-import { CtaCardProps, ListProps } from "@voltmoney/schema";
+import { CtaCardProps, ListProps, TypographyProps } from "@voltmoney/schema";
 import {
   customEditPortfolio,
   portfolioListDatastoreBuilder,
 } from "../portfolio/utils";
-import sharedPropsService from "../../../SharedPropsService";
-import {getTotalLimit} from "../portfolio/actions";
+import SharedPropsService from "../../../SharedPropsService";
+import { getDesiredValue } from "../portfolio_readonly/actions";
+import { addCommasToNumber } from "../../../configs/utils";
+
 let amount: number = 0;
 
 export const ModifyAmountAction: ActionFunction<ModifyAmountPayload> = async (
@@ -15,7 +17,7 @@ export const ModifyAmountAction: ActionFunction<ModifyAmountPayload> = async (
   _datastore,
   { setDatastore, goBack }
 ): Promise<any> => {
-  const stepResponseObject = action.payload.stepResponseObject
+  const stepResponseObject = action.payload.stepResponseObject;
   await customEditPortfolio(
     action.payload.index,
     amount,
@@ -31,6 +33,21 @@ export const ModifyAmountAction: ActionFunction<ModifyAmountPayload> = async (
   });
   await setDatastore(ROUTE.PORTFOLIO, "totalItem", <CtaCardProps>{
     ...props.totalItem,
+  });
+
+  const portValue = getDesiredValue(
+    action.payload.stepResponseObject.availableCAS,
+    action.payload.stepResponseObject.isinNAVMap
+  );
+
+  await SharedPropsService.setDesiredPortfolio(portValue);
+
+  await setDatastore(ROUTE.PORTFOLIO, "subTitleText", <TypographyProps>{
+    label: `₹${addCommasToNumber(portValue)} out of ₹${addCommasToNumber(
+      parseInt(
+        action.payload.stepResponseObject["totalPortfolioAmount"].toString()
+      )
+    )} are selected for pledging.`,
   });
 
   await goBack();
