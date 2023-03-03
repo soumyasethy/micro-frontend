a4f2a79a9104d5b190f3f91987f4ae18d180c295
Arpit Luniya <arpit.luniya@voltmoney.in>
2023-03-03 17:38:39 +0530

    Fixing KYC summary onload duplicate calls

--- src/features/kyc/kyc_summary/actions.ts
+++ src/features/kyc/kyc_summary/actions.ts
@@ -1,102 +1,108 @@
-import { ActionFunction } from "@voltmoney/types";
-import SharedPropsService from "../../../SharedPropsService";
+import { ActionFunction } from '@voltmoney/types'
+import SharedPropsService from '../../../SharedPropsService'
 import {
-  AadharInitPayload,
-  NavigationNext,
-  ToggleKYCVerifyCTA,
-} from "../kyc_init/types";
+    AadharInitPayload,
+    NavigationNext,
+    ToggleKYCVerifyCTA,
+} from '../kyc_init/types'
 import {
-  ButtonProps,
-  ButtonTypeTokens,
-  StepperStateToken,
-} from "@voltmoney/schema";
-import { ROUTE } from "../../../routes";
-import { ACTION } from "./types";
-import _ from "lodash";
-import { api } from "../../../configs/api";
-import { APP_CONFIG, getAppHeader } from "../../../configs/config";
-import { User } from "../../login/otp_verify/types";
-import {AnalyticsEventTracker, TextConstants} from "../../../configs/constants";
+    ButtonProps,
+    ButtonTypeTokens,
+    StepperStateToken,
+} from '@voltmoney/schema'
+import { ROUTE } from '../../../routes'
+import { ACTION } from './types'
+import _ from 'lodash'
+import { api } from '../../../configs/api'
+import { APP_CONFIG, getAppHeader } from '../../../configs/config'
+import { User } from '../../login/otp_verify/types'
+import {
+    AnalyticsEventTracker,
+    TextConstants,
+} from '../../../configs/constants'
 
 export const verifyKycSummary: ActionFunction<any> = async (
-  action,
-  _datastore,
-  { network, setDatastore, showPopup, analytics }
+    action,
+    _datastore,
+    { network, setDatastore, showPopup, analytics },
 ): Promise<any> => {
-  await setDatastore(ROUTE.KYC_SUMMARY, "continue", <ButtonProps>{
-    loading: true,
-  });
-  const applicationId = (await SharedPropsService.getUser())
-    .linkedApplications[0].applicationId;
+    await setDatastore(ROUTE.KYC_SUMMARY, 'continue', <ButtonProps>{
+        loading: true,
+    })
+    const applicationId = (await SharedPropsService.getUser())
+        .linkedApplications[0].applicationId
 
-  const response = await network.get(
-    `${api.kycSummaryVerify}${applicationId}`,
-    { headers: await getAppHeader() }
-  );
-  await setDatastore(ROUTE.KYC_SUMMARY, "continue", <ButtonProps>{
-    loading: false,
-  });
-  if (_.get(response, "data.updatedApplicationObj.currentStepId", false)) {
-    const user: User = await SharedPropsService.getUser();
-    user.linkedApplications[0].stepStatusMap.KYC_SUMMARY =
-      StepperStateToken.COMPLETED;
-    user.linkedApplications[0].stepStatusMap.KYC_AADHAAR_VERIFICATION =
-      StepperStateToken.COMPLETED;
-    user.linkedApplications[0].stepStatusMap.KYC_PHOTO_VERIFICATION =
-      StepperStateToken.COMPLETED;
-    user.linkedApplications[0].stepStatusMap.BANK_ACCOUNT_VERIFICATION =
-      StepperStateToken.IN_PROGRESS;
-    await SharedPropsService.setUser(user);
+    const response = await network.get(
+        `${api.kycSummaryVerify}${applicationId}`,
+        { headers: await getAppHeader() },
+    )
+    await setDatastore(ROUTE.KYC_SUMMARY, 'continue', <ButtonProps>{
+        loading: false,
+    })
+    if (_.get(response, 'data.updatedApplicationObj.currentStepId', false)) {
+        const user: User = await SharedPropsService.getUser()
+        user.linkedApplications[0].stepStatusMap.KYC_SUMMARY =
+            StepperStateToken.COMPLETED
+        user.linkedApplications[0].stepStatusMap.KYC_AADHAAR_VERIFICATION =
+            StepperStateToken.COMPLETED
+        user.linkedApplications[0].stepStatusMap.KYC_PHOTO_VERIFICATION =
+            StepperStateToken.COMPLETED
+        user.linkedApplications[0].stepStatusMap.BANK_ACCOUNT_VERIFICATION =
+            StepperStateToken.IN_PROGRESS
+        await SharedPropsService.setUser(user)
 
-    analytics(AnalyticsEventTracker.borrower_kyc_complete["Event Name"], {
-      ...AnalyticsEventTracker.borrower_kyc_complete,
-    });
+        analytics(AnalyticsEventTracker.borrower_kyc_complete['Event Name'], {
+            ...AnalyticsEventTracker.borrower_kyc_complete,
+        })
 
-    await showPopup({
-      autoTriggerTimerInMilliseconds: APP_CONFIG.AUTO_REDIRECT,
-      isAutoTriggerCta: false,
-      title: "KYC done successfully!",
-      subTitle: TextConstants.GENERIC_PROCEED_MESSAGE,
-      type: "SUCCESS",
-      ctaLabel: "Continue",
-      primary: true,
-      ctaAction: {
-        type: ACTION.NAVIGATION_NEXT,
-        routeId: ROUTE.KYC_SUMMARY,
-        payload: <NavigationNext>{
-          stepId: _.get(response, "data.updatedApplicationObj.currentStepId"),
-        },
-      },
-    });
-  }
-};
+        await showPopup({
+            autoTriggerTimerInMilliseconds: APP_CONFIG.AUTO_REDIRECT,
+            isAutoTriggerCta: false,
+            title: 'KYC done successfully!',
+            subTitle: TextConstants.GENERIC_PROCEED_MESSAGE,
+            type: 'SUCCESS',
+            ctaLabel: 'Continue',
+            primary: true,
+            ctaAction: {
+                type: ACTION.NAVIGATION_NEXT,
+                routeId: ROUTE.KYC_SUMMARY,
+                payload: <NavigationNext>{
+                    stepId: _.get(
+                        response,
+                        'data.updatedApplicationObj.currentStepId',
+                    ),
+                },
+            },
+        })
+    }
+}
 
 export const NavigateNext: ActionFunction<NavigationNext> = async (
-  action,
-  _datastore,
-  { navigate, goBack }
+    action,
+    _datastore,
+    { navigate, goBack },
 ): Promise<any> => {
-  await goBack();
-  if (action.payload.stepId) await navigate(action.payload.stepId);
-};
+    // await goBack();
+    if (action.payload.stepId) await navigate(action.payload.stepId)
+}
 
 export const GoBackAction: ActionFunction<AadharInitPayload> = async (
-  action,
-  _datastore,
-  { goBack, navigate }
+    action,
+    _datastore,
+    { goBack, navigate },
 ): Promise<any> => {
-  // await goBack();
-  await navigate(ROUTE.KYC_STEPPER);
-};
+    // await goBack();
+    await navigate(ROUTE.KYC_STEPPER)
+}
 
 export const ToggleKYCSummaryCTA: ActionFunction<ToggleKYCVerifyCTA> = async (
-  action,
-  _datastore,
-  { setDatastore }
+    action,
+    _datastore,
+    { setDatastore },
 ): Promise<any> => {
-  await setDatastore(ROUTE.KYC_SUMMARY, "continue", <ButtonProps>{
-    type: action.payload.value
-      ? ButtonTypeTokens.LargeFilled
-      : ButtonTypeTokens.LargeOutline,
-  });
-};
+    await setDatastore(ROUTE.KYC_SUMMARY, 'continue', <ButtonProps>{
+        type: action.payload.value
+            ? ButtonTypeTokens.LargeFilled
+            : ButtonTypeTokens.LargeOutline,
+    })
+}
