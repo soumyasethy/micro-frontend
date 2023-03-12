93b6236dc2a68f142d9f89bb9d45da2825bbe0e9
Arpit Luniya <arpit.luniya@voltmoney.in>
2023-03-12 19:27:26 +0530

    Correcting url getParam function

--- src/configs/uri-config-utils.ts
+++ src/configs/uri-config-utils.ts
@@ -18,11 +18,11 @@ export const getParameters: (url: string) => {
     let paramString = url.split('?')[1]
 
     let queryString = new URLSearchParams(paramString)
-    console.log('Query =>', queryString)
     for (let pair of queryString.entries()) {
         params[pair[0].includes('/') ? pair[0].split('/')[1] : pair[0]] =
             pair[1].includes('/') ? pair[1].split('/')[0] : pair[1]
     }
+    console.log('Query =>', params, queryString)
     return params
 }
 
@@ -39,7 +39,7 @@ export const isGoogleLoginEnabled = (user: User) => {
 
 export const shouldShowVoltBrandingSplashScreen = (url: string) => {
     const params = getParameters(url)
-    if (params[QUERY_PARAMS.PLATFORM] === AppPlatform.SDK_INVESTWELL) {
+    if (params[QUERY_PARAMS.PLATFORM].includes(AppPlatform.SDK_INVESTWELL)) {
         return false
     } else {
         return true
@@ -48,7 +48,7 @@ export const shouldShowVoltBrandingSplashScreen = (url: string) => {
 
 export const areWhatsappUpdatesAllowed = (url: string) => {
     const params = getParameters(url)
-    if (params[QUERY_PARAMS.PLATFORM] === AppPlatform.SDK_INVESTWELL) {
+    if (params[QUERY_PARAMS.PLATFORM].includes(AppPlatform.SDK_INVESTWELL)) {
         return false
     } else {
         return true
@@ -57,7 +57,7 @@ export const areWhatsappUpdatesAllowed = (url: string) => {
 
 export const shouldShowVoltContactUs = (url: string) => {
     const params = getParameters(url)
-    if (params[QUERY_PARAMS.PLATFORM] === AppPlatform.SDK_INVESTWELL) {
+    if (params[QUERY_PARAMS.PLATFORM].includes(AppPlatform.SDK_INVESTWELL)) {
         return false
     } else {
         return true
